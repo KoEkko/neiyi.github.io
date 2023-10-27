f5362fbb719168e4118131bcc9425ff668cc2d5a
KoEkko <2251930460@qq.com>
2023-10-27 15:12:17 +0800

    feat

---
+++ docs/static/hon_7-5dc8a474.js
@@ -0,0 +1,1 @@
+const t="/static/hon_3-d0eb8291.jpg",s="/static/hon_1-7e2644e3.jpg",o="/static/hon_4-e8d8db28.jpg",_="/static/hon_5-cbcfdfbb.jpg",c="/static/hon_6-ab90e223.jpg",a="/static/hon_7-ff0cd389.jpg";export{t as _,s as a,o as b,_ as c,c as d,a as e};
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "docs"), // Equivalent to assetsRoot in webpack
+    assetsDir: "static", // Equivalent to assetsSubDirectory in webpack
+    // ...
+  },
+
   plugins: [
     vue(),
     AutoImport({
